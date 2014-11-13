397c6520265270bea09d5e4cd865a3f330e0ce1e
Graeme Gott <graeme@gottcode.org>
2014-11-13 17:02:51 -0500

    Update translations.

---
+++ translations/hexalate_he.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="he" sourcelanguage="en">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="../src/board.cpp" line="45"/>
+        <source>Question</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/board.cpp" line="45"/>
+        <source>Abort current game?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="50"/>
+        <source>Select application language:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="53"/>
+        <source>&lt;System Language&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="183"/>
+        <source>Note</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="183"/>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Puzzle</name>
+    <message>
+        <location filename="../src/puzzle.cpp" line="162"/>
+        <source>Success</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/puzzle.cpp" line="204"/>
+        <source>Sorry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/puzzle.cpp" line="204"/>
+        <source>Unable to load previous game. Starting a new game.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../src/window.cpp" line="33"/>
+        <location filename="../src/window.cpp" line="77"/>
+        <source>Hexalate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="40"/>
+        <source>&amp;Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="41"/>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="43"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="46"/>
+        <source>&amp;Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="47"/>
+        <source>Application &amp;Language...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="49"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="50"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="52"/>
+        <source>About &amp;Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="76"/>
+        <source>About Hexalate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="78"/>
+        <source>A color matching game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="79"/>
+        <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="80"/>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/hexalate_lt.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt" sourcelanguage="en">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="../src/board.cpp" line="45"/>
+        <source>Question</source>
+        <translation>Klausimas</translation>
+    </message>
+    <message>
+        <location filename="../src/board.cpp" line="45"/>
+        <source>Abort current game?</source>
+        <translation>Nutraukti dabartinį žaidimą?</translation>
+    </message>
+</context>
+<context>
+    <name>LocaleDialog</name>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="50"/>
+        <source>Select application language:</source>
+        <translation>Pasirinkite programos kalbą:</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="53"/>
+        <source>&lt;System Language&gt;</source>
+        <translation>&lt;Sistemos Kalba&gt;</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="183"/>
+        <source>Note</source>
+        <translation>Pastaba</translation>
+    </message>
+    <message>
+        <location filename="../src/locale_dialog.cpp" line="183"/>
+        <source>Please restart this application for the change in language to take effect.</source>
+        <translation>Prašome paleisti programą iš naujo, kad įsigaliotų kalbos pakeitimai.</translation>
+    </message>
+</context>
+<context>
+    <name>Puzzle</name>
+    <message>
+        <location filename="../src/puzzle.cpp" line="162"/>
+        <source>Success</source>
+        <translation>Sekmė</translation>
+    </message>
+    <message>
+        <location filename="../src/puzzle.cpp" line="204"/>
+        <source>Sorry</source>
+        <translation>Deja</translation>
+    </message>
+    <message>
+        <location filename="../src/puzzle.cpp" line="204"/>
+        <source>Unable to load previous game. Starting a new game.</source>
+        <translation>Neįmanoma įkelti ankstesnio žaidimo. Pradedamas naujas žaidimas.</translation>
+    </message>
+</context>
+<context>
+    <name>Window</name>
+    <message>
+        <location filename="../src/window.cpp" line="33"/>
+        <location filename="../src/window.cpp" line="77"/>
+        <source>Hexalate</source>
+        <translation>Hexalate</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="40"/>
+        <source>&amp;Game</source>
+        <translation>&amp;Žaidimas</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="41"/>
+        <source>&amp;New</source>
+        <translation>&amp;Naujas</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="43"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Baigti</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="46"/>
+        <source>&amp;Settings</source>
+        <translation>&amp;Nustatymai</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="47"/>
+        <source>Application &amp;Language...</source>
+        <translation>Programos Kalba</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="49"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Pagalba</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="50"/>
+        <source>&amp;About</source>
+        <translation>&amp;Apie</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="52"/>
+        <source>About &amp;Qt</source>
+        <translation>Apie &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="76"/>
+        <source>About Hexalate</source>
+        <translation>Apie Hexalate</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="78"/>
+        <source>A color matching game</source>
+        <translation>Spalvų sutapimo žaidimas</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="79"/>
+        <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
+        <translation>Autorinės Teisės &amp;copy; 2009-%1 Graeme Gott</translation>
+    </message>
+    <message>
+        <location filename="../src/window.cpp" line="80"/>
+        <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
+        <translation>Išleista pagal &lt;a href=%1&gt;GPL 3&lt;/a&gt; licenciją</translation>
+    </message>
+</context>
+</TS>
