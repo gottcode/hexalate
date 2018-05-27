ce711a977eb40f07f83f6f42ce55bcd9a7d7ae8f
Graeme Gott <graeme@gottcode.org>
2018-05-27 07:42:41 -0400

    Sync translations with Transifex.

--- translations/hexalate_pt.ts
+++ translations/hexalate_pt.ts
@@ -1,49 +1,49 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl" sourcelanguage="en">
+<TS version="2.1" language="pt" sourcelanguage="en">
 <context>
     <name>Board</name>
     <message>
         <source>Question</source>
-        <translation>Vraag</translation>
+        <translation>Questão</translation>
     </message>
     <message>
         <source>Abort current game?</source>
-        <translation>Huidig spel afbreken?</translation>
+        <translation>Abortar o jogo atual?</translation>
     </message>
 </context>
 <context>
     <name>LocaleDialog</name>
     <message>
         <source>Select application language:</source>
-        <translation>Taal selecteren:</translation>
+        <translation>Selecione o idioma da aplicação:</translation>
     </message>
     <message>
         <source>&lt;System Language&gt;</source>
-        <translation>&lt;System Language&gt;</translation>
+        <translation>&lt;Idioma do Sistema&gt;</translation>
     </message>
     <message>
         <source>Note</source>
-        <translation>Opmerking</translation>
+        <translation>Nota</translation>
     </message>
     <message>
         <source>Please restart this application for the change in language to take effect.</source>
-        <translation>Herstart de applicatie om de taalwijziging toe te passen.</translation>
+        <translation>Por favor reinicie a aplicação para a alteração ao idioma surtir efeito.</translation>
     </message>
 </context>
 <context>
     <name>Puzzle</name>
     <message>
         <source>Success</source>
-        <translation>Voltooid</translation>
+        <translation>Sucesso</translation>
     </message>
     <message>
         <source>Sorry</source>
-        <translation>Sorry</translation>
+        <translation>Desculpe</translation>
     </message>
     <message>
         <source>Unable to load previous game. Starting a new game.</source>
-        <translation>Het vorige spel kan niet worden geladen; start een nieuw spel.</translation>
+        <translation>Não foi possível carregar o jogo anterior. A começar um novo jogo.</translation>
     </message>
 </context>
 <context>
@@ -54,51 +54,51 @@
     </message>
     <message>
         <source>&amp;Game</source>
-        <translation>&amp;Spel</translation>
+        <translation>&amp;Jogo</translation>
     </message>
     <message>
         <source>&amp;New</source>
-        <translation>&amp;Nieuw</translation>
+        <translation>&amp;Novo</translation>
     </message>
     <message>
         <source>&amp;Quit</source>
-        <translation>&amp;Afsluiten</translation>
+        <translation>&amp;Sair</translation>
     </message>
     <message>
         <source>&amp;Settings</source>
-        <translation>In&amp;stellingen</translation>
+        <translation>&amp;Definições</translation>
     </message>
     <message>
         <source>Application &amp;Language...</source>
-        <translation>Applicatietaa&amp;l...</translation>
+        <translation>&amp;Idioma da Aplicação...</translation>
     </message>
     <message>
         <source>&amp;Help</source>
-        <translation>&amp;Hulp</translation>
+        <translation>&amp;Ajuda</translation>
     </message>
     <message>
         <source>&amp;About</source>
-        <translation>&amp;Over</translation>
+        <translation>S&amp;obre</translation>
     </message>
     <message>
         <source>About &amp;Qt</source>
-        <translation>Over &amp;Qt</translation>
+        <translation>Sobre o &amp;Qt</translation>
     </message>
     <message>
         <source>About Hexalate</source>
-        <translation>Over Hexalate</translation>
+        <translation>Sobre o Hexalate</translation>
     </message>
     <message>
         <source>A color matching game</source>
-        <translation>Een kleurenkoppelspel</translation>
+        <translation>Um jogo de correspondência de cores</translation>
     </message>
     <message>
         <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
-        <translation>Auteursrecht &amp;copie; 2009-%1 Graeme Gott</translation>
+        <translation>Direitos de autor &amp;copy; 2009-%1 Graeme Gott</translation>
     </message>
     <message>
         <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
-        <translation>Uitgebracht onder de &lt;a href=%1&gt;GPL 3&lt;/a&gt;-licentie</translation>
+        <translation>Lançado sob a licença &lt;a href=%1&gt;GPL 3&lt;/a&gt;</translation>
     </message>
 </context>
 </TS>
--- translations/hexalate_zh.ts
+++ translations/hexalate_zh.ts
@@ -1,49 +1,49 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="nl" sourcelanguage="en">
+<TS version="2.1" language="zh" sourcelanguage="en">
 <context>
     <name>Board</name>
     <message>
         <source>Question</source>
-        <translation>Vraag</translation>
+        <translation>问题</translation>
     </message>
     <message>
         <source>Abort current game?</source>
-        <translation>Huidig spel afbreken?</translation>
+        <translation>终止当前游戏？</translation>
     </message>
 </context>
 <context>
     <name>LocaleDialog</name>
     <message>
         <source>Select application language:</source>
-        <translation>Taal selecteren:</translation>
+        <translation>选择本游戏的语言:</translation>
     </message>
     <message>
         <source>&lt;System Language&gt;</source>
-        <translation>&lt;System Language&gt;</translation>
+        <translation>&lt;系统语言&gt;</translation>
     </message>
     <message>
         <source>Note</source>
-        <translation>Opmerking</translation>
+        <translation>提示</translation>
     </message>
     <message>
         <source>Please restart this application for the change in language to take effect.</source>
-        <translation>Herstart de applicatie om de taalwijziging toe te passen.</translation>
+        <translation>请重启本程序使语言设置生效。</translation>
     </message>
 </context>
 <context>
     <name>Puzzle</name>
     <message>
         <source>Success</source>
-        <translation>Voltooid</translation>
+        <translation>恭喜，你成功了！</translation>
     </message>
     <message>
         <source>Sorry</source>
-        <translation>Sorry</translation>
+        <translation>抱歉</translation>
     </message>
     <message>
         <source>Unable to load previous game. Starting a new game.</source>
-        <translation>Het vorige spel kan niet worden geladen; start een nieuw spel.</translation>
+        <translation>无法加载上次游戏。正在开始新游戏。</translation>
     </message>
 </context>
 <context>
@@ -54,51 +54,51 @@
     </message>
     <message>
         <source>&amp;Game</source>
-        <translation>&amp;Spel</translation>
+        <translation>&amp;游戏</translation>
     </message>
     <message>
         <source>&amp;New</source>
-        <translation>&amp;Nieuw</translation>
+        <translation>&amp;新游戏</translation>
     </message>
     <message>
         <source>&amp;Quit</source>
-        <translation>&amp;Afsluiten</translation>
+        <translation>&amp;退出程序</translation>
     </message>
     <message>
         <source>&amp;Settings</source>
-        <translation>In&amp;stellingen</translation>
+        <translation>&amp;设置</translation>
     </message>
     <message>
         <source>Application &amp;Language...</source>
-        <translation>Applicatietaa&amp;l...</translation>
+        <translation>程序 &amp;语言...</translation>
     </message>
     <message>
         <source>&amp;Help</source>
-        <translation>&amp;Hulp</translation>
+        <translation>&amp;帮助</translation>
     </message>
     <message>
         <source>&amp;About</source>
-        <translation>&amp;Over</translation>
+        <translation>&amp;关于</translation>
     </message>
     <message>
         <source>About &amp;Qt</source>
-        <translation>Over &amp;Qt</translation>
+        <translation>关于 &amp;Qt</translation>
     </message>
     <message>
         <source>About Hexalate</source>
-        <translation>Over Hexalate</translation>
+        <translation>关于 Hexalate</translation>
     </message>
     <message>
         <source>A color matching game</source>
-        <translation>Een kleurenkoppelspel</translation>
+        <translation>一个颜色搭配游戏</translation>
     </message>
     <message>
         <source>Copyright &amp;copy; 2009-%1 Graeme Gott</source>
-        <translation>Auteursrecht &amp;copie; 2009-%1 Graeme Gott</translation>
+        <translation>版权所有 &amp;copy; 2009-%1 Graeme Gott</translation>
     </message>
     <message>
         <source>Released under the &lt;a href=%1&gt;GPL 3&lt;/a&gt; license</source>
-        <translation>Uitgebracht onder de &lt;a href=%1&gt;GPL 3&lt;/a&gt;-licentie</translation>
+        <translation>基于 &lt;a href=%1&gt;GPL 3&lt;/a&gt; 协议发布</translation>
     </message>
 </context>
 </TS>
